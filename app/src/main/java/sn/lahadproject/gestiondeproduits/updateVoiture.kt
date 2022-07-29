16b1e88b7d675ff0de0e9bcb093d9ae6316a7ced
Abdou Lahad SAMB <100447448+lahadsamb28@users.noreply.github.com>
2022-07-29 02:24:23 +0000

    update not fixed

---
+++ app/src/main/java/sn/lahadproject/gestiondeproduits/updateVoiture.kt
@@ -0,0 +1,11 @@
+package sn.lahadproject.gestiondeproduits
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class updateVoiture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+    }
+}
